bbd86b00c115b24678a642624a490d18529013b1
Ananthakrishnan96 <aravind07021996@gmail.com>
2016-09-09 18:38:03 +0530

    Create alpah.c

---
+++ alpah.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    char n;
+    printf("Enter any alphabet");
+    scanf("%c",&n);
+    if(n==&n)
+    {
+        printf("you entered the alphabet");
+    }
+    else
+    {
+        printf("It is not alphabet");
+    }
+}
